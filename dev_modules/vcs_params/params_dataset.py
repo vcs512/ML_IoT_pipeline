890df5019eb928370fa0a52b10f8e7709873147c
vcs512 <vcsprofissional@gmail.com>
2023-08-05 20:49:09 -0300

    EDA - Data insights, unbalance and sets checking

---
+++ dev_modules/vcs_params/params_dataset.py
@@ -0,0 +1,13 @@
+# Dataset directories parameters.
+
+# System path location.
+DATASET_ROOT_DIR = "1_dataset"
+DATASET_TYPE = "resize_128"
+
+# Already divided paths.
+TRAIN_DIR = "train"
+TEST_DIR = "test"
+
+# Classes dirs.
+USABLE_DIR = "0_utilizavel"
+DEFECTIVE_DIR = "1_defeituoso"
